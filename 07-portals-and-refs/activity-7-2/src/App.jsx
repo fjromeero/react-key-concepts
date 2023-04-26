dce3ad1e670dd8812d6ab808fdc3c8c7fb62433b
fjromeero <franjaviro2001@gmail.com>
2023-04-26 13:15:31 +0200

    feat: Añade ejercicio portales
    Signed-off-by: fjromeero <franjaviro2001@gmail.com>

---
+++ 07-portals-and-refs/activity-7-2/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import MainNavigation from './components/MainNavigation'
+
+function App() {
+  return (
+    <>
+      <MainNavigation/>
+      <div id='drawer'></div>
+    </>
+  )
+}
+
+export default App
